3e4abe1ed5d8f3685d2823452d067c5ae5ed47ae
Gopinath Vijayarangan <Gopinath Vijayarangan>
2017-02-01 20:04:56 -0600

    table sequence changed

--- database/migrations/2017_01_29_000005_create_Res_contact_table.php
+++ database/migrations/2017_01_29_000005_create_Res_contact_table.php
@@ -27,7 +27,7 @@ class CreateResContactTable extends Migration
         });
 
         Schema::table('res_contact', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
